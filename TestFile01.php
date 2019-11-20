52712cc1110339f8fa3a258719d137c91b03fda4
debtuli <debtuli@gmail.com>
2019-11-20 15:16:12 +0530

    Update TestFile01.php

--- TestFile01.php
+++ TestFile01.php
@@ -1,10 +1,9 @@
 <html>
  <head>
-  <title>PHP Test</title>
- <h1 style="color:blue;">This is a heading</h1>
+  
  </head>
  <body>
-  <p style="color:red;">This is a paragraph.</p>
+  
  <?php 
 Print "Hello, World!";
 ?>
